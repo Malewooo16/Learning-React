788b0d53ce0139b17c90de41f3c61a4a2751b479
Malewo16 <125474294+Malewooo16@users.noreply.github.com>
2023-07-14 11:14:35 +0000

    Update 3 files

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,12 +1,45 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
+  //   constructor(){
+  //     super();
+  //     this.handleIncrement=this.handleIncrement.bind(this);
+
+  //   }
+  handleIncrement = (ide) => {
+    console.log(ide)
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  doHandleIncrement=() =>{
+    this.handleIncrement({id:1});
+  }
   render() {
     return (
       <div>
-        <h1>Hello World</h1> <button>Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.doHandleIncrement}
+          className="btn btn-secondary btm-sm"
+        >
+          Increment
+        </button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let clas = "badge m-2 bg-";
+    clas += this.state.count === 0 ? "warning" : "primary";
+    return clas;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
