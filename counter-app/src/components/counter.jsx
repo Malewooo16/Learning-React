314d20a2dff22279c4dfd19e7b0279d90a07a6d8
Malewo16 <125474294+Malewooo16@users.noreply.github.com>
2023-07-14 23:35:40 +0000

    Update 3 files

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -47,22 +47,22 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = { 
-    count:0,
+    value: this.props.value
    } 
 
    handleIncrement=()=>{
-    this.setState({count: this.state.count + 1})
+    this.setState({value: this.state.value + 1})
     
    }
 
    
    formatCount(){
-    let {count}= this.state;
+    let {value: count}= this.state;
     return count===0 ? "Zero" : count;
    }
    getBadgeClasses(){
     let classes="badge m-2 bg-";
-    classes+=this.state.count===0 ? "warning":"primary";
+    classes+=this.state.value===0 ? "warning":"primary";
     return classes;
    }
   render() { 
